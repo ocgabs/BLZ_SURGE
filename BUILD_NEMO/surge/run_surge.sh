c7fefe5663dccea7aebdf5b5843ec32fbbb74805
thopri <thopri@fedora-33.lan>
2021-02-10 07:47:55 +0000

    inital commit of surge-container changes

---
+++ BUILD_NEMO/surge/run_surge.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Linking NEMO exe"
+ln -s -f /SRC/NEMOGCM/CONFIG/BLZ_SURGE/BLD/bin/nemo.exe .
+sleep 1
+echo "Linking XIOS server"
+ln -s -f /SRC/XIOS/bin/xios_server.exe .
+sleep 1
+echo "Linking Data INPUTS"
+ln -s -f INPUTS bdydta
+sleep 1
+echo "Running Model Now"
+mpirun -n 2 ./nemo.exe : -n 1 ./xios_server.exe
